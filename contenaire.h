4d85550de5f38ba78197a25c640355ef7221d615
Adalbert Pungu <18pw419@esisalama.org>
2022-03-07 23:19:39 +0200

    Primitives

--- contenaire.h
+++ contenaire.h
@@ -16,3 +16,10 @@ typedef struct objet{
 } Objet;
 
 Objet tab[Dim];
+
+void createContenaire(Objet tab[], int n ); 
+void ajoutContenaire(Objet tab[], int n, Complex c );
+void supprimerContenaire(Objet tab[], int n , Complex  ob );
+void detruireContenaire(Objet tab[], int n );
+Complex  valeurContenaire(Objet tab[], int n );
+void printContenaire(Objet tab[], int n );
