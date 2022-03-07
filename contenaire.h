b7e0bbb66733d3b934f28ad2d53bf91541f719ab
Adalbert Pungu <18pw419@esisalama.org>
2022-03-07 23:18:34 +0200

    Ajout des structures

--- contenaire.h
+++ contenaire.h
@@ -3,3 +3,16 @@
 #include <stdbool.h>
 #include <math.h>
 
+#define Dim 10
+
+typedef struct complex{
+	double r; // real part
+	double i; // imag part
+}Complex;
+
+typedef struct objet{
+	Complex c; 
+	bool b; 
+} Objet;
+
+Objet tab[Dim];
