5192a4040894e19d5fc9bca2d1c2d8c7ca16a1a4
Adalbert Pungu <18pw419@esisalama.org>
2022-03-07 22:18:00 +0200

    Les primitives

--- complex.h
+++ complex.h
@@ -12,3 +12,20 @@ typedef struct complex{
   float i; // Partiie Imaginaire
 } complex;
 
+// Les primitives
+
+complex C_new (double r, double i);
+
+complex C_add (complex a,  complex b);
+
+complex C_mul (complex a,  complex b);
+
+double  C_module (complex c);
+
+bool C_compare (complex a, complex b);
+
+/* Fonction d'affichage d'un nombre complexe */
+
+void C_print(complex a);
+
+
