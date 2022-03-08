66757a40c5c48581417109175030f3773c64b077
Adalbert Pungu <18pw419@esisalama.org>
2022-03-08 10:58:26 +0200

    update main.c

--- tas/main.c
+++ tas/main.c
@@ -4,15 +4,13 @@ int main(int argc, char const *argv[])
 {
   TasMin t;
   creerTas(&t, 'D');
-  printf("%c\n", valeur(&t));
   entasser(&t, 'C');
-  printf("%c\n", valeur(&t));
   entasser(&t, 'B');
-  printf("%c\n", valeur(&t));
   entasser(&t, 'A');
-  printf("%c\n", valeur(&t));
   entasser(&t, 'G');
+  supprimer(&t);
+  supprimer(&t);
   printf("%c\n", valeur(&t)); 
-
+  
   return 0;
 }
