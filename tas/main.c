f78dd3ba9d1b173661d0ab08abd331cb0f991899
Adalbert Pungu <18pw419@esisalama.org>
2022-03-08 09:51:41 +0200

    teste des primitives

--- tas/main.c
+++ tas/main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char const *argv[])
 {
-  
+  TasMin t;
+  creerTas(&t, 'D');
+  printf("%c\n", valeur(&t));
+  entasser(&t, 'C');
+  printf("%c\n", valeur(&t));
+  entasser(&t, 'B');
+  printf("%c\n", valeur(&t));
+  entasser(&t, 'A');
+  printf("%c\n", valeur(&t));
+  entasser(&t, 'G');
+  printf("%c\n", valeur(&t)); 
+
   return 0;
 }
