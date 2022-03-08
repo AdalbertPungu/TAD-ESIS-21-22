7ef6f195b91f8c804cf529b1900479954c3a27ab
Adalbert Pungu <18pw419@esisalama.org>
2022-03-08 09:51:11 +0200

    teste des primitives

--- tas/tas.h
+++ tas/tas.h
@@ -13,7 +13,7 @@ typedef struct TasMin
 void creerTas(TasMin *t, char x);
 char valeur(TasMin *t);
 void entasser(TasMin *t, char x);
-
+void supprimer(TasMin *t);
 void reorganiserAsc(TasMin *t, int s);
 char pere(TasMin *t, int s);
 char permuter(TasMin *t, int s2);
