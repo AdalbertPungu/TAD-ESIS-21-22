9a83616e30cb40584dd0f383277902acddef431a
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 01:26:16 +0200

    Add the structs

--- ListeSC/interfaceListeSC.h
+++ ListeSC/interfaceListeSC.h
@@ -6,3 +6,18 @@
 #define TAILLEMaX 15     
 #define NIL -1
 
+typedef int curseur;
+
+typedef struct cellule{
+  char val;
+  curseur idSuiv;
+} Cellule;
+
+typedef struct listSC{
+  int taille;
+  Cellule tab[TAILLEMaX];
+  curseur premier;
+  curseur cle;
+  curseur prLibre;
+}listSC;
+
