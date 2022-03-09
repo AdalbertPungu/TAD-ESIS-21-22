8b029d3aea007da2125c13ce413e4bf18f7756a7
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 00:36:27 +0200

    Test of primitives

--- pile/main.c
+++ pile/main.c
@@ -8,7 +8,22 @@ int main(int argc, char const *argv[])
 
   creerPile(&p);
   
-  
+  empiler(&p, 'a');
+  empiler(&p, 'b');
+  empiler(&p, 'c');
+  empiler(&p, 'g');
+
+  empiler(&p, 'a');
+  empiler(&p, 'b');
+  empiler(&p, 'c');
+  empiler(&p, 'g');
+
+  empiler(&p, 'a');
+  empiler(&p, 'b');
+  empiler(&p, 'c');
+  empiler(&p, 'g');
+
+  printf("%c", valeur(&p));
 
   return 0;
 }
