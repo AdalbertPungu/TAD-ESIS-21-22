40d3b701b04915d44630101a1553380eea9fc64b
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 01:35:20 +0200

    update creerListe function

--- ListeSC/interfaceListeSC.c
+++ ListeSC/interfaceListeSC.c
@@ -1,5 +1,16 @@
 #include "interfaceListeSC.h"
 
 void creerListe(listSC *L) {
-    
+  L->taille = 0;
+  L->premier = NIL;
+  L->cle = NIL;
+  L->prLibre = 0;
+  
+  int i;
+  
+  for (i=0; i<TAILLEMaX-1; i++) {
+    L->tab[i].idSuiv = i+1;
+  }
+
+  L->tab[i].idSuiv = NIL; 
 }
