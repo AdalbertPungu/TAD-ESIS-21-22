97f62246540ca5cfa4d90f7ee5f534c46fab1726
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 03:42:36 +0200

    Add the primitives

---
+++ arbre/arbre.h
@@ -0,0 +1,20 @@
+
+
+sommet creerArbreBinaire(objet racine);
+void detruireSommet(sommet s);
+void* getValeur(sommet s);
+void setValeur(sommet s, objet o);
+sommet filsGauche(sommet S);
+sommet filsDroit(sommet S);
+sommet pere(sommet S);
+void ajouterFilsGauche(sommet s, objet o);
+void ajouterFilsDroit(sommet s, objet o);
+void supprimerFilsGauche(sommet s);
+void supprimerFilsDroit(sommet s);
+void detruitSommet(sommet *n);
+
+//Nouvelles Primitives
+void setFilsGauche(sommet s, sommet fg);
+void setFilsDroit(sommet s, sommet fd);
+void setPere(sommet s,sommet p);
+
