1625074cf719be34a37eb921d1221873c7b495ee
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 02:19:57 +0200

    Add tranStrToLSC  function

--- ListeSC/utilitaire.c
+++ ListeSC/utilitaire.c
@@ -1,2 +1,12 @@
 #include "interfaceListeSC.c"
 
+listSC tranStrToLSC(char *s) {
+  listSC L;
+  creerListe(&L);
+  int i;
+  for (i=strlen(s)-1; i>=0; i--) {
+    insereEnTete(&L, s[i]);
+  }
+  return L;
+}
+
