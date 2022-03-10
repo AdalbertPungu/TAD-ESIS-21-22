67141472e36d42001bf879ba30b44cb7d8c7e72c
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 02:31:27 +0200

    Tests of primitives

--- ListeSC/main.c
+++ ListeSC/main.c
@@ -2,6 +2,32 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  listSC list;
+
+  creerListe(&list);
+  
+  insereEnTete(&list, 'A');
+  insereEnTete(&list, 'B');
+  insereEnTete(&list, 'C');
+  insereEnTete(&list, 'D');
+  
+  suivant(&list);
+  suivant(&list);
+  
+  insereApres(&list, 'F');
+  supprimeEnTete(&list);
+  insereEnTete(&list, 'C');
+  insereEnTete(&list, 'G');
+  
+  afficherListe(&list);
+  supprimeApres(&list);
+  afficherListe(&list);
+  
+  suivant(&list);
+  suivant(&list);
+  
+  supprimeApres(&list);
+  afficherListe(&list);
+
   return 0;
 }
