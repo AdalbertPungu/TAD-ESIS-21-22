356cc7689cd6e7210faf442b8d1ba654ab834afc
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 02:43:30 +0200

    Add creerCellule function

--- ListeDC/interfaceLDC.c
+++ ListeDC/interfaceLDC.c
@@ -1,2 +1,13 @@
 #include "interfaceLDC.h"
 
+curseur creerCellule(char c)
+{
+    curseur cel = (curseur)malloc(sizeof(Cellule));
+    if (cel != NIL) {
+        cel->v = c;
+        cel->celSuiv = NIL;
+        cel->celPrec = NIL;
+    }
+    return cel;
+}
+
