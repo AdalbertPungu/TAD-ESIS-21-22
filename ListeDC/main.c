2a08bf1dbc15b105931bc8c5cfa169c59336f806
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 03:00:40 +0200

    Test of primitives

--- ListeDC/main.c
+++ ListeDC/main.c
@@ -2,6 +2,29 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+
+  LDC l = creerLDC();
+  
+  insererEntete(l, 'x');
+  insererEntete(l, 'A');
+  insererApres(l, 'G');
+  insererApres(l, 'P');
+
+  printf("Taille : %d\n", l->taille);
+  printf("Entete : %c\n", l->celEntete->v);
+  printf("Cle : %c\n", l->cle->v);
+  printf("Queue : %c\n", l->celEnqueue->v);
+
+  printf("Suivant Entete : %c\n", l->celEntete->celSuiv->v);
+  printf("Précedant Queue : %c\n", l->celEnqueue->celPrec->v);
+
+  // insererEntete(l, 'T');
+  // insererEntete(l, 'P');
+
+  // printf("taille : %d\n", l->taille);
+  // printf("tete : %c\n", l->celEntete->v);
+  // printf("taille : %c\n", l->celEntete->celSuiv->v);
+  // printf("taille : %c\n", l->celEnqueue->v);
+
   return 0;
 }
