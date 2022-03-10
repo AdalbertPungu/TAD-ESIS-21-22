0c60b82ced20947c39f8489ec9d97913dd1a53c1
Adalbert Pungu <18pw419@esisalama.org>
2022-03-10 02:37:50 +0200

    the structs

--- ListeDC/interfaceLDC.h
+++ ListeDC/interfaceLDC.h
@@ -2,3 +2,26 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define NIL 0
+
+typedef struct Cellule Cellule;
+typedef struct Cellule *curseur;
+
+typedef struct ListDC ListDC;
+typedef struct ListDC *LDC;
+
+struct Cellule
+{
+    char v;
+    curseur celSuiv;
+    curseur celPrec;
+};
+
+struct ListDC
+{
+    curseur celEntete;
+    curseur celEnqueue;
+    int taille;
+    curseur cle;
+};
+
